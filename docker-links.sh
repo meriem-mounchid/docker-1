a29d4994d1697dd60d2de94303df5e4550167af9
Meriem Mounchid <mmounchi@e1r9p11.1337.ma>
2020-01-14 10:01:00 +0100

    done

---
+++ docker-links.sh
@@ -0,0 +1,4 @@
+rm -rf ~/.docker ~/Library/Containers/com.docker.docker
+mkdir ~/goinfre/.docker ~/goinfre/com.docker.docker
+ln -s ~/goinfre/.docker ~/.docker
+ln -s ~/goinfre/com.docker.docker ~/Library/Containers/com.docker.docker
